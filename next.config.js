b7aefdceda8aea3c3584896727941483508dd18b
Timur <timurrahmatullaev703@gmail.com>
2023-09-25 19:53:14 +0600

    a

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'cdn.freecodecamp.org',
+            port: '',
+            pathname: '/curriculum/css-photo-gallery/**',
+          },
+        ],
+      },
+}
 
 module.exports = nextConfig
